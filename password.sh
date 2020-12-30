21f9d4178f224073f931ba0abdcc065c3d06b8a6
Charmander <~@charmander.me>
2020-12-30 03:09:17 -0800

    Configure SCRAM authentication only on PostgreSQL that supports it

--- password.sh
+++ password.sh
@@ -6,11 +6,23 @@ cat >|"$PGDATA/pg_hba.conf" <<-'END'
 
 	host all postgres all trust
 	host all postgres_password all password
-	host all scram_test all scram-sha-256
 END
 
-psql -v ON_ERROR_STOP=1 <<-'END'
+psql -v ON_ERROR_STOP=1 <<-'SQL'
 	CREATE ROLE postgres_password SUPERUSER LOGIN PASSWORD 'test-password';
-	SET password_encryption = 'scram-sha-256';
-	CREATE ROLE scram_test LOGIN PASSWORD 'test4scram';
-END
+
+	DO $$
+		DECLARE
+			version integer;
+		BEGIN
+			SHOW server_version_num INTO version;
+
+			IF version >= 100000 THEN
+				SET password_encryption = 'scram-sha-256';
+				CREATE ROLE scram_test LOGIN PASSWORD 'test4scram';
+				CREATE EXTENSION IF NOT EXISTS adminpack;
+				PERFORM pg_file_write('pg_hba.conf', E'host all scram_test all scram-sha-256\n', true);
+			END IF;
+		END
+	$$;
+SQL
